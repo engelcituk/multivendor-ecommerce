4b3ada4f2d95f6a6915b9ddba9e97f7c904e8821
eCituk <citukcaamal@gmail.com>
2025-12-15 20:03:36 -0500

    fix para ejecutar seeds

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -20,6 +20,16 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->runningInConsole() && ! $this->app->runningUnitTests()) {
+
+        // No intentes cargar la configuración si estamos migrando
+        // o si estamos en cualquier otro comando de Artisan
+        // que no sea un test.
+        // Hacemos una excepción: si el comando es 'config:clear', DEBE ejecutarse.
+        if (request()->server('argv')[1] !== 'config:clear') {
+            return;
+        }
+    }
         $settings = $this->app->make(SettingService::class);
         $settings->setSettings();
     }
